a2de13177d9763a63d815d427c32552383ab5361
kairavishah2001 <kairavishah2001@gmail.com>
2024-02-29 13:13:31 -0700

    Cleaning done

--- Travel Website/script.js
+++ Travel Website/script.js
@@ -33,6 +33,7 @@ function prev(){
     index = (index - 1 + slides.length) % slides.length;
     slides[index].classList.add('active');
 }
+
 function openLightbox() {
     var lightbox = document.getElementById('lightbox');
     lightbox.style.display = 'block';
